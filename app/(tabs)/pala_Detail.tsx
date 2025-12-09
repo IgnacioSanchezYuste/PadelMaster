514a6cde2ea74b476d6d8a4b5d3b118b264f9ea9
pabloburonmoron <pabloburon2006@gmail.com>
2025-12-09 13:27:18 +0100

    creacion de detalle de las palas

---
+++ app/(tabs)/pala_Detail.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Link } from 'expo-router';
+
+
+
+import ApiExample from "../FetchPalas";
+import ApiPelotas from '../FetchPalas copy';
+
+export default function DetallePala() {
+return (
+ <SafeAreaProvider style={{ backgroundColor: '#25292e', paddingTop: 50 }}>
+     <SafeAreaView style={styles.safeArea}>
+      <ApiExample />
+       
+     </SafeAreaView>
+   </SafeAreaProvider>
+);
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+
+});
