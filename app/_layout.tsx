3b52b22c932b2a7f15c4bca5d39d56f9169c50d3
IgnacioSanchezYuste <ignaciosanchezyuste@gmail.com>
2026-04-21 08:47:39 +0200

    cositas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,5 @@
-import { Stack } from 'expo-router';
+import { Slot } from 'expo-router';
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
-  );
+  return <Slot />;
 }
